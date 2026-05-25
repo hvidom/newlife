131c0cc80ff75621bc0358206e9e6f48b2aac182
Hryhorii Victorovich Ilin <devopsick@gmail.com>
2026-05-25 23:26:04 +0100

    perfect biome and build mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 import { siteConfig } from "./src/config/site";
 
 const SITE = import.meta.env.PROD ? siteConfig.url : "http://localhost:4321";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), sitemap()],
-  adapter: cloudflare(),
-  site: SITE,
+	integrations: [react(), mdx(), sitemap()],
+	adapter: cloudflare(),
+	site: SITE,
 	output: "server",
 	prefetch: {
 		prefetchAll: true,
@@ -21,10 +21,10 @@ export default defineConfig({
 	server: {
 		port: 4321,
 	},
-  experimental: {
-    advancedRouting: true,
-  },
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	experimental: {
+		advancedRouting: true,
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
